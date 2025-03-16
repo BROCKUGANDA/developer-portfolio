edc37df1797bad65693c1efb5073eabbdca963ef
BROCKUGANDA <otemaach@gmail.com>
2025-03-16 19:29:44 +0300

    Optimize for Netlify deployment

--- next.config.js
+++ next.config.js
@@ -7,5 +7,9 @@ module.exports = {
   images: {
     domains: ['res.cloudinary.com', 'media.dev.to', 'media2.dev.to'],
     unoptimized: true
-  }
+  },
+  output: 'export',
+  distDir: 'out',
+  // Ensure that Next.js works well with Netlify
+  trailingSlash: true
 }
