ccead1771defd77f4b56ddf1715202d56b1d49dd
BROCKUGANDA <otemaach@gmail.com>
2025-03-18 10:28:46 +0300

    Initial commit with portfolio projects

--- utils/data/projects-data.js
+++ utils/data/projects-data.js
@@ -7,8 +7,30 @@ export const projectsData = [
         role: 'Full Stack Developer',
         code: 'https://github.com/BROCKUGANDA/tailwind-landing-page-template',
         demo: 'https://tailwind-landing-page-template-brockuganda.vercel.app/',
+    },
+    {
+        id: 2,
+        name: "Real Estate Website",
+        description: "Lizron Real Estate has been helping clients find their dream homes for over 15 years. Our dedicated team of experts provides personalized service to meet all your real estate needs. Whether you're buying, selling, or investing, we're committed to making your real estate journey smooth and successful.",
+        tools: ["React", "Node.js", "MongoDB"],
+        role: 'Full Stack Developer',
+        code: "https://github.com/BROCKUGANDA/Real-estate",
+        demo: "https://lizron.netlify.app/",
     }
 ];
+
+// Do not remove any property.
+// Leave it blank instead as shown below
+
+// {
+//     id: 1,
+//     name: '',
+//     description: "",
+//     tools: [],
+//     role: '',
+//     code: '',
+//     demo: '',
+// }
 
 // Do not remove any property.
 // Leave it blank instead as shown below
