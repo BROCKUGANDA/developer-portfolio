9d1cb0e4d430dbdcba1a2805b648f16eeb171f90
BROCKUGANDA <100225083+BROCKUGANDA@users.noreply.github.com>
2025-04-04 08:51:31 +0300

    Update favicon configuration in layout

--- app/layout.js
+++ app/layout.js
@@ -13,6 +13,14 @@ export const metadata = {
   title: "Portfolio of Otema - Software Developer",
   description:
     "This is the portfolio of Otema. I am a full stack developer and a self taught developer. I love to learn new things and I am always open to collaborating with others. I am a quick learner and I am always looking for new challenges.",
+  icons: {
+    icon: [
+      { url: '/favicon.ico' },
+    ],
+    apple: [
+      { url: '/favicon.ico' },
+    ],
+  },
 };
 
 export default function RootLayout({ children }) {
