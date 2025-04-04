1236d6fe3ee64ec98fe74d295f9a93e6be438ede
BROCKUGANDA <100225083+BROCKUGANDA@users.noreply.github.com>
2025-04-04 08:57:36 +0300

    Comprehensive update to favicon and icon configuration

--- app/layout.js
+++ app/layout.js
@@ -13,12 +13,14 @@ export const metadata = {
   title: "Portfolio of Otema - Software Developer",
   description:
     "This is the portfolio of Otema. I am a full stack developer and a self taught developer. I love to learn new things and I am always open to collaborating with others. I am a quick learner and I am always looking for new challenges.",
+  manifest: "/manifest.json",
   icons: {
     icon: [
-      { url: '/favicon.ico' },
+      { url: '/icon.png', type: 'image/png' },
+      { url: '/crane.jpg', type: 'image/jpeg' }
     ],
     apple: [
-      { url: '/favicon.ico' },
+      { url: '/crane.jpg', type: 'image/jpeg' }
     ],
   },
 };
@@ -26,6 +28,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/icon.png" />
+        <link rel="shortcut icon" href="/icon.png" />
+        <link rel="apple-touch-icon" href="/crane.jpg" />
+      </head>
       <body className={inter.className}>
         <ToastContainer />
         <main className="min-h-screen relative mx-auto px-6 sm:px-12 lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[92rem] text-white">
